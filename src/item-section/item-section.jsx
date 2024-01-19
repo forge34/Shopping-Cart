83d75eb850e2f10fd47281adb62f7dc28ddd1421
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2024-01-19 17:18:35 +0200

    added item section & it's style

--- src/item-section/item-section.jsx
+++ src/item-section/item-section.jsx
@@ -1,4 +1,16 @@
+import Card from "../item-card/item-card";
+import styles from "./item-section.module.css"
+
 export default function ItemSection(params) {
-    return <>
-    </>
+  return (
+    <div className={styles.container}>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </div>
+  );
 }
