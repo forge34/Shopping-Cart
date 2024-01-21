9cf4ebd130de78c783658bbd97346af2d1f30343
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2024-01-21 14:39:41 +0200

    added loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,32 @@ import Header from "./Header/header";
 import style from "./common-styles/app.module.css";
 import useData from "./hooks/useData";
 import { Outlet } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 const App = () => {
   const [ItemNumber, setNumber] = useState(0);
   const [cartItems, setCart] = useState([]);
+  const [data, isLoading, error] = useData();
 
-  function handleAdd(amount, name, price,url) {
-    console.log(url)
+  function handleAdd(amount, name, price, url) {
+    console.log(url);
     setCart([
       ...cartItems,
       {
         name: name,
         price: price,
-        url:url,
+        url: url,
       },
     ]);
     setNumber((prev) => prev + amount);
   }
 
-  const context = [handleAdd, useData(), cartItems];
+  const context = [handleAdd, data, cartItems];
 
   return (
     <div className={style.container}>
       <Header ItemNumber={ItemNumber}></Header>
-      <Outlet context={context}></Outlet>
+      {isLoading ? <ClipLoader loading={isLoading}></ClipLoader> : <Outlet context={context}></Outlet>}
     </div>
   );
 };
